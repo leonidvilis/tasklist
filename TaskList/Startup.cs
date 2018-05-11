a1de572903f7bc85d7d0d9754f7201040778485c
lvlsv <idleo@yandex.ru>
2018-05-11 09:05:36 +0300

    Added to bitbucket

---
+++ TaskList/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TaskList.Startup))]
+namespace TaskList
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
